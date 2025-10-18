0b944c488dc2aff2ab52cc36140cc4de44a305d1
Mitchell <mitchell_wang@outlook.com>
2025-10-19 00:12:14 +1100

    implemented image fetch and display + laoding, error, empty state

--- frontend-ui/src/app/components/itemViewer.tsx
+++ frontend-ui/src/app/components/itemViewer.tsx
@@ -1,6 +1,32 @@
 "use client"
 
 import React, { useEffect, useState } from "react";
+import { styled } from '@mui/system';
+
+const Container = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "100vh",
+});
+
+const Title = styled("h1")({
+  color: "white",
+  fontSize: "2rem",
+  fontWeight: "700",
+  textDecoration: "underline",
+  marginBottom: "1rem",
+});
+
+const Image = styled("img")({
+  maxWidth: "100%",
+  height: "auto",
+});
+
+const State = styled("div")({
+  marginTop: "1rem",
+});
 
 export default function ItemViewer() {
   const [images, setImages] = useState<string[]>([]);
@@ -30,17 +56,16 @@ export default function ItemViewer() {
 
     fetchImages();
   }, []);
-
   const current = images[index];
 
   return (
-    <div>
-        <h1>Dogs Images</h1>
-        {loading && <p>Loading...</p>}
-        {error && <p>Error: Something went wrong</p>}
-        {!loading && !error && images.length === 0 && <p>No images available</p>}
-        {current && <img src={current} alt="Dog" />}
-    </div>
+    <Container>
+        <Title>Dogs Images</Title>
+        {loading && <State>Loading...</State>}
+        {error && <State>Error: Something went wrong</State>}
+        {!loading && !error && images.length === 0 && <State>No images available</State>}
+        {current && <Image src={current} alt="Dog" />}
+    </Container>
   );
 }
 
